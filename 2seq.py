695afc0a2f4545a1b4cd800b6f9d32aa086f7a5e
DigRea <146025236+DigRea@users.noreply.github.com>
2023-11-23 23:20:30 +0300

    2seq.py

--- 2seq.py
+++ 2seq.py
@@ -1,4 +1,10 @@
 line = input('Введите цифры через запятую: ')
-lst = eval(line)
-
+if ',' in line:
+    spr = ','
+elif ';' in line:
+    spr = ';'
+else:
+    spr = '/'
+lst = line.split(spr)
+lst = set(lst)
 print(lst)
